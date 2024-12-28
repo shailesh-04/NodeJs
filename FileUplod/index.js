ff21f0f9187584d13d1bd1f79164dfce7c95f1a6
shailesh-04 <shailesakoli@gmail.com>
2024-12-28 11:53:16 +0530

    File Upload Project

---
+++ FileUplod/index.js
@@ -0,0 +1,51 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  if (req.url == '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<form action="/fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write('Error parsing the file');
+        return res.end();
+      }
+      var oldpath = files.filetoupload[0].filepath;
+      var newpath = __dirname + '/' + files.filetoupload[0].originalFilename;
+      fs.copyFile(oldpath, newpath, function (err) {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.write('Error uploading the file');
+          return res.end();
+        }
+        fs.unlink(oldpath, function (err) {
+          if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.write('Error cleaning up temporary file');
+            return res.end();
+          }
+
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.write('File uploaded and moved!');
+          res.end();
+        });
+      });
+    });
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
