96a9846a3ca0d428cfd9f6f28014c4488a849e24
shailesh-04 <shailesakoli@gmail.com>
2024-12-28 12:32:33 +0530

    Ok

--- FileUplod/index.js
+++ FileUplod/index.js
@@ -4,16 +4,19 @@ var fs = require('fs');
 http.createServer(function (req, res) {
   if (req.url == '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Using Form Submit</h1>');
     res.write('<form action="/fileupload" method="post" enctype="multipart/form-data">');
     res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="text" name="userName"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
     return res.end();
   }
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
-
+    
     form.parse(req, function (err, fields, files) {
+        console.log(fields);
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.write('Error parsing the file');
